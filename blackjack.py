fb35c13795947596a4549cf0ce47cefef9ce2dbf
Aaron Brooks <abrooks@mastermnd.io>
2020-03-09 19:26:29 -0400

    Black Jack needs our help!

---
+++ blackjack.py
@@ -0,0 +1,22 @@
+class Card:
+    #The init constructor should accept the card suit, and the cards value 
+    
+    #Create a constructor to return the string representation of your card. 
+
+class Deck:
+    #The init contructor should create a deck of cards, using our card class
+
+    #To make sure everything is fair, Mr. Black needs a way to shuffle the deck
+    
+    #As the dealer, Mr. Black needs to be able to deal cards to players
+
+class Hand:
+    #Lets initiate the hand here. Dont forget to account for the dealer
+        
+    #The Reset of the class functions are up to you!
+
+class Moola:
+    #Jack like the stakes to be high. He doesnt play without betting, and you gotta play until someone loses it all....Good luck
+
+
+#GAME LOGIC GOES HERE. You could use a class, but lets work out the logic first. 
